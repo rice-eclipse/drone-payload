8c3d082883241edfa63cd2566434ab9b8df17408
deepsn458 <84109373+deepsn458@users.noreply.github.com>
2023-03-10 20:39:49 -0600

    rc override code
    code for rc override when limit switches are activated

---
+++ rc_override.py
@@ -0,0 +1,39 @@
+import RPI.GPIO as GPIO
+from dronekit import connect
+
+#Vehicle's address
+ADDRESS = '/dev/ttyS0'
+
+#On and off signal frequencies
+SIGNAL_ON = 1200
+SIGNAL_OFF = 1700
+
+#Pins connected to switches
+#TODO
+INPUT_PIN1 = 3
+INPUT_PIN2 = 5
+
+#sets up the pins connected to the switches
+GPIO.setup(INPUT_PIN1, GPIO.IN)
+GPIO.setup(INPUT_PIN2, GPIO.IN)
+
+#connects to the vehicle
+vehicle = connect(ADDRESS, wait_ready=True, baud=57600)
+def signal_detection():
+    
+    # loops continuously
+    while(True):
+
+        #sends the 1200Hz signal to channel 7 if either of the switches are on
+        if (GPIO.input(INPUT_PIN1) or GPIO.input(INPUT_PIN2)):
+            vehicle.channels.overrides = {'7', SIGNAL_ON}
+
+        # sends the 1700Hz signal to channel switches if both switches are off
+        else:
+            vehicle.channels.overrides = {'7', SIGNAL_OFF}
+    
+    return
+
+if __name__ == "main":
+    signal_detection()
+    
