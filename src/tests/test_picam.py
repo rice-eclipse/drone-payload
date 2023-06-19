dabacda15fb4ba401ccb5c571329c3f804cae5ab
irrundle3 <irrundle3@github.com>
2023-06-19 14:07:55 -0600

    camera test no preview

--- src/tests/test_picam.py
+++ src/tests/test_picam.py
@@ -9,11 +9,6 @@ from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
 
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.configure(preview_config)
-
-picam2.start_preview(Preview.QTGL)
-
 picam2.start()
 time.sleep(2)
 
