3f03d34353377386dc1560e4e0d1744f2713326d
irrundle3 <irrundle3@github.com>
2023-06-20 08:23:09 -0600

    camera tests

--- src/tests/test_picam.py
+++ src/tests/test_picam.py
@@ -8,15 +8,30 @@ import datetime
 
 from picamera2 import Picamera2
 
+running = False
+def test_sig(future):
+    running = False
+
 picam2 = Picamera2()
-
+picam2.camera.set_controls({"AfMode": libcamera.controls.AfModeEnum.Continuous})
 picam2.start()
-time.sleep(2)
 
-metadata = picam2.capture_file("test.jpg", wait=False)
-print(metadata)
+print("job 1")
 time.sleep(2)
-metadata = picam2.capture_file("test2.jpg")
-print(metadata)
+running = True
+job = picam2.capture_file("test.jpg", wait=False, signal_function=test_sig)
+while running:
+    print("running!")
+    time.sleep(0.5)
+print("job1:", job.get_result())
 
+print("job 2")
+time.sleep(2)
+running = True
+job = picam2.capture_file("test2.jpg", wait=False, signal_function=test_sig)
+while running:
+    print("running!")
+    time.sleep(0.5)
+print("job 2 finished capture")
+print(job.get_result())
 picam2.close()
