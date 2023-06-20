b248ef7cdac90ca1760469dd57af01e25cc0f167
irrundle3 <irrundle3@github.com>
2023-06-19 22:59:26 -0600

    i am an absolute bufoon (baffoon?)

--- src/lib/capturing.py
+++ src/lib/capturing.py
@@ -36,9 +36,7 @@ class Camera:
         Retrieves a metadata dictionary from photo taking
         '''
         self.proc_running = True
-        def no_proc_running():
-            self.proc_running = False
-        self._job = self.camera.capture_file(path, wait=False, signal_function=no_proc_running)
+        self._job = self.camera.capture_file(path, wait=False, signal_function=self._photo_complete)
     
     def get_job_results(self):
         if not self.proc_running:
@@ -54,6 +52,9 @@ class Camera:
     
     def close(self):
         self.camera.close()
+    
+    def _photo_complete(self, job):
+        self.proc_running = False
 
 def chessboard_images(n_samples: int) -> List[ChessboardImage]:
     result = []
